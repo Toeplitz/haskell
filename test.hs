15b9b005fbcc65168f7b0d460b8c143801fdaf60
Martin Sarajærvi <balony@gmail.com>
2014-01-24 23:29:27 +0100

    testing

--- test.hs
+++ test.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveFoldable #-}
+
 import qualified Data.Traversable as Tr
 import qualified Data.Foldable as Fl
 import Control.Monad
 import Control.Applicative
+
 
 data Test = Test { desc  :: String
                  , value :: Int
@@ -10,34 +15,39 @@ data Test = Test { desc  :: String
 
 data Data t = Data { foo :: t 
                    , bar :: t       
-} 
+                   , baz :: t
+} deriving (Functor, Tr.Traversable, Fl.Foldable)
 
-exampleData = Data { foo = Test "foo" 1 
+
+exampleData = Data { foo = Test "Foo" 1 
                    , bar = Test "Bar" 2
-}
+                   , baz = Test "Baz" 3
+} 
 
 instance Show Test where
   show f = (desc f) ++ ": " ++ (show $ value f)
 
-instance (Show a) => Show (Data a) where
-  show f = show (foo f)
 
-instance Functor Data where
-  fmap = Tr.fmapDefault
+--instance Functor Data where
+--  fmap = Tr.fmapDefault
 
-instance Fl.Foldable Data where
-  foldMap = Tr.foldMapDefault
+--instance Fl.Foldable Data where
+--  foldMap = Tr.foldMapDefault
 
-instance Tr.Traversable Data where
-    traverse f = Data f  -- Try to show a Test entry inside the Data structure
-
+--instance Tr.Traversable Data where
+--    traverse f (Data a a') = Data <$> f a <*> f a'
 --  
 --  traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
 --
 
+ms :: Test 
+ms x =  "lol"
+
+main :: IO()
 main = do
-  putStrLn $ show exampleData
-  Tr.traverse (putStrLn show) exampleData
+  --putStrLn $ show exampleData
+  Tr.traverse print exampleData
+  Tr.traverse (ms) exampleData
 
 -- OBJECTIVE:
 -- Traverse exampleData, printing "foo" and "bar"
